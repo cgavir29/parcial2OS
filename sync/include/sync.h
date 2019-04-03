e8b7e34f71085ec3788d8924da97af01a4d43637
Camilo Gaviria <camilogaviria@Camilos-MacBook-Pro.local>
2019-04-03 11:13:38 -0500

    Avances Sync.h

--- sync/include/sync.h
+++ sync/include/sync.h
@@ -2,4 +2,17 @@
 #include "isync.h"
 #include <semaphore.h>
 
-
+class Sync : public ISync{
+    public:
+        Sync(const char *name, bool open) ;
+        ~Sync();
+        ISync* create(const char *name);
+        ISync* open(const char *name);
+        void destroy(const char *name);
+        void wait();
+        void signal();
+        void close();
+    private:
+        const char *name;
+        // bool open;
+};
