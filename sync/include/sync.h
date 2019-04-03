0529847ca97ab9a243e9be33dbb12d629e2e418e
Camilo Gaviria <camilogaviria@Camilos-MacBook-Pro.local>
2019-04-03 11:25:23 -0500

    Modificaciones Sync.h

--- sync/include/sync.h
+++ sync/include/sync.h
@@ -6,9 +6,9 @@ class Sync : public ISync{
     public:
         Sync(const char *name, bool open) ;
         ~Sync();
-        ISync* create(const char *name);
-        ISync* open(const char *name);
-        void destroy(const char *name);
+        static ISync* create(const char *name);
+        static ISync* open(const char *name);
+        static void destroy(const char *name);
         void wait();
         void signal();
         void close();
