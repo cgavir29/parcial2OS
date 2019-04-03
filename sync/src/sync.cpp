4c582e8c04d5bbacb85a143230d3170fdd63ea6e
Camilo Gaviria <camilogaviria@Camilos-MacBook-Pro.local>
2019-04-03 12:02:27 -0500

    Comentarios

--- sync/src/sync.cpp
+++ sync/src/sync.cpp
@@ -12,13 +12,17 @@ Sync::~Sync() {
 }
 
 ISync* Sync::create(const char *name) {
-  return new Sync(name, true);
+  return new Sync(name, false);
 }
 
 ISync* Sync::open(const char *name) {
+  // obtener el objeto ISync que se creo en en Sync::create
+  // por medio del 'name', retornar este objeto cambiando open a true
 }
 
 void Sync::destroy(const char *name) {
+  // determinar la instancia de ISync que tiene nombre 'name'
+  // e invocar el destructor en este
 }
 
 void Sync::wait() {
